a94812554a0a01d99b5e7247c0bab20cd6f2049c
Kira Kutscher <kira@tailorsoft.de>
2022-10-20 19:02:39 +0200

    refactored code to use jqueryUI for the dialogs. It's soooo much nicer!

--- barnum.js
+++ barnum.js
@@ -70,7 +70,7 @@ function get_n_random_statements(n) {
 
     let n_statements = "";
     for (let i = 0; i < n; i++) {
-        n_statements = n_statements.concat(statements_array[i], "\n\n")
+        n_statements = n_statements.concat(statements_array[i], "<br/> <br/>")
     }
 
     return n_statements;
@@ -83,65 +83,124 @@ function remove_animation() {
 
 function get_results () {
 
+    console.log("Calculating results!"); 
     let result = get_n_random_statements(7);
-    let accuracy = prompt("Your results are here!\n\n"+
-                          result+
-                          "\nOn a scale of 1-5, where 5 is highly accurate and 1 is not accurate at all,"+
-                          " how would you rate your results?", "");
+    $("#results").html(result+
+                      "<br/><br/>On a scale of 1-5, where 5 is highly accurate and 1 is not accurate at all,"+
+                      " how would you rate your results? <br/>");
 
-    let confirmation = false;
-    if (accuracy != null && accuracy != "") {
-        accuracy = parseFloat(accuracy);
-    }
+    // Working here. 
+    dialog_form.dialog( "open" ); 
 
-    if (accuracy >= 0
-        && accuracy < 2) {
-        confirmation = confirm("Oh, you are good! Did you realise that I tried to trick you?\n\n"+
-                              "If you want to know what exactly I tried, you can click \"OK\" on this dialog.");
-    }
-    else if (accuracy >= 2 && accuracy < 3.75) {
-        confirmation = confirm("Well, I could have done better, I guess...\n\n"+
-                               "But not in measuring your personality, because I didn't actually do that. " +
-                               "I used fully general statements in order to make you feel "+
-                               "as if I understood you. \n\n"+
-                               "If you want to know more about this technique and how to avoid falling "+
-                               "for it, click on \"OK\".");
-    }
-    else if (accuracy >= 3.75) {
-        confirmation = confirm("Ha! Gotcha! :-) \n\n"+
-                               "This was all just a hoax, I didn't actually measure anything.\n\n"+
-                               "Please click on \"OK\", and I'll take you to some more information "+
-                               "as well as some good tips on how to avoid falling for this tactic.");
-        
-
-    }
-
-    else {
-        confirmation = confirm("Okay, you got me! This isn't research at all! \n\n"+
-                               "I was trying to trick you by using statements that are "+
-                               "so general that nearly everybody will agree with them.\n\n"+
-                               "Do you want to learn more about this?");
-
-    }
-
-    if (confirmation) {
-        console.log("you clicked ok");
-        window.location="./explanation.html";
-    }
 }
 
+function receive_rating () {
+    let accuracy = $("#rating").val();
+    $("#rating").val("");
+    let rating_dialog = $("#after_rating");
+    
+    if (accuracy != null && accuracy != "") {
+        accuracy = parseFloat(accuracy);
+        
+        if (accuracy >= 0
+            && accuracy < 2) {
+            rating_dialog.html("Oh, huh! So bad? Did you realise that I tried to trick you?" +
+                               "<br/><br/>"+
+                               "If you want to know what I tried, please click on \"Learn more!\"");
+        }
+        else if (accuracy >= 2 && accuracy < 3.75) {
+            rating_dialog.html("Well, I could have done better, I guess...<br/><br/>"+
+                               "But not in measuring your personality, because I didn't actually do that. " +
+                               "I used fully general statements in order to make you feel "+
+                               "as if I understood you.<br/><br/>"+
+                               "If you don't know about the Barnum effect, please click on \"Learn more!\"");
+        }
+        else if (accuracy >= 3.75) {
+            rating_dialog.html("Ha! Gotcha! :-) <br/><br/>"+
+                               "This was all just a hoax, I didn't actually measure anything.<br/><br/>"+
+                               "Please click on \"Learn more!\", and I'll take you to some more information "+
+                               "as well as some good tips on how to avoid falling for this tactic.");
+            
+
+        }
+        else {
+            rating_dialog.html("Okay, you got me! This isn't research at all! <br/><br/>"+
+                               "I was trying to trick you by using statements that are "+
+                               "so general that nearly everybody will agree with them.<br/><br/>"+
+                               "Do you want to learn more about this?");
+        }
+    } else {
+        rating_dialog.html("Okay, you got me! This isn't research at all! <br/><br/>"+
+                           "I was trying to trick you by using statements that are "+
+                           "so general that nearly everybody will agree with them.<br/><br/>"+
+                           "Do you want to learn more about this?");
+    }
+
+    rating_dialog.dialog( "open" );
+    
+}
+
+function rate () {
+    dialog_form.dialog("close");
+    receive_rating(); 
+}
+
+
+function learn_more () {
+    window.location=("./explanation.html"); 
+}
+
+
 function test () {
-    window.location="./explanation.html"; 
+    get_results(); 
 }
 
 
 function start_scan () {
     console.log("Starting scan.");
-
     
     $("#measure-square").addClass("scanimation");
     let t1 = setTimeout(remove_animation, 10000);
-
+    
     let t2 = setTimeout(get_results, 10000); 
     
 }
+
+
+$( function () {
+    $("#rating").val("");
+    
+    dialog_form = $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 650,
+        width: 500,
+        modal: true,
+        position: {my: "left", at: "left+10%"},
+        buttons: {Rate: rate,
+                  Cancel : function () {
+                      dialog_form.dialog("close");
+                      receive_rating (); }
+                 }
+    });
+
+    dialog_form.find( "form" ).on( "submit", function (event) {
+        event.preventDefault();
+        rate();
+    });
+
+    
+    rating_dialog = $( "#after_rating" ).dialog({
+        autoOpen: false,
+        height: 650,
+        width: 500,
+        modal: true,
+        position: {my: "left", at: "left+10%"},
+        buttons: { "Learn more!" : learn_more,
+                  Cancel : function () { rating_dialog.dialog("close");}
+                 },
+        close: function ( event, ui) {
+            $("#rating").val(""); 
+        }
+    });
+
+});
